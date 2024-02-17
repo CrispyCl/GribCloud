36df5e8558fe46369174bb28efbc624cecb2ffbc
Maksuma <max.bakurin@gmail.com>
2024-02-17 18:16:02 +0400

    init frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className='flex justify-center'>
+        <h1 className='text-5xl font-bold'>Hello world!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/vite.config.ts
@@ -0,0 +1,18 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@assets': '/src/assets',
+      '@components': '/src/components',
+      '@hooks': '/src/hooks',
+      '@pages': '/src/pages',
+      '@store': '/src/store',
+      '@utils': '/utils',
+    },
+  },
+})
