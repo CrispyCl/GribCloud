e22f2fd1c91e8c1e71dc3837259fef6eccea5aec
Gogasmerter <chebunin19@gmail.com>
2024-02-22 04:47:23 +0400

    Add JWT authentication

--- backend/GribCloud/GribCloud/urls.py
+++ backend/GribCloud/GribCloud/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
+    path("api/v1/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/v1/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("admin/", admin.site.urls),
 ]
