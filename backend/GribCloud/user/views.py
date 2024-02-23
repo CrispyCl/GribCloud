d8f9b4175f1f6102cd9d75349c08816a9cd63ecf
Gogasmerter <chebunin19@gmail.com>
2024-02-23 09:24:03 +0400

    Add current user retrieve

--- backend/GribCloud/user/views.py
+++ backend/GribCloud/user/views.py
@@ -1,4 +1,6 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from core.permissions import IsSelfOrReadOnly
 from user.models import User
@@ -14,3 +16,11 @@ class UserAPIDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (IsSelfOrReadOnly,)
+
+
+class UserAPICurrent(generics.RetrieveAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
