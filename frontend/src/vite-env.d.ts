970b669ddb58d2d94cfdeabef88ca0ecb6d2453c
Maksuma <max.bakurin@gmail.com>
2024-02-25 00:24:46 +0400

    Update .env Add Firebase config

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -2,6 +2,12 @@
 
 interface ImportMetaEnv {
   readonly VITE_APP_API_URL: string
+  readonly VITE_FIREBASE_API_KEY: string
+  readonly VITE_FIREBASE_AUTH_DOMAIN: string
+  readonly VITE_FIREBASE_PROJECT_ID: string
+  readonly VITE_FIREBASE_STORAGE_BUCKET: string
+  readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string
+  readonly VITE_FIREBASE_APP_ID: string
 }
 
 interface ImportMeta {
