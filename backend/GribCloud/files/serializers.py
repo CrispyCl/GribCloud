babc4b73adb5a2c97b6d39cbbf2b1773937f0837
Gogasmerter <chebunin19@gmail.com>
2024-03-01 12:36:23 +0400

    Add File tests

--- backend/GribCloud/files/serializers.py
+++ backend/GribCloud/files/serializers.py
@@ -2,22 +2,61 @@ from django.utils.translation import pgettext_lazy
 from rest_framework import serializers
 
 from files.models import File
+from user.models import User
 
 
 class FileCreateSerializer(serializers.Serializer):
     files = serializers.ListField(child=serializers.CharField())
+    author = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), required=False)
+
+    def validate(self, data):
+        request = self.context.get("request")
+        if request:
+            user = request.user
+            if not user:
+                user = data.get("author")
+                if not user:
+                    raise serializers.ValidationError(
+                        {
+                            "author": pgettext_lazy(
+                                "File Create Serializer validation error no author",
+                                "Author not provided",
+                            ),
+                        },
+                        code="required",
+                        field="author",
+                    )
+        else:
+            user = data.get("author")
+            if not user:
+                raise serializers.ValidationError(
+                    {
+                        "author": pgettext_lazy(
+                            "File Create Serializer validation error no author",
+                            "Author not provided",
+                        ),
+                    },
+                    code="required",
+                )
+        if not data.get("files"):
+            raise serializers.ValidationError(
+                {
+                    "files": pgettext_lazy(
+                        "File Create Serializer validation error no files",
+                        "No files provided",
+                    ),
+                },
+                code="required",
+            )
+        return data
 
     def create(self, validated_data):
         request = self.context.get("request")
-        user = request.user
+        if request:
+            user = request.user
+        else:
+            user = validated_data.get("author")
         files = validated_data.pop("files")
-        if not files:
-            raise serializers.ValidationError(
-                pgettext_lazy(
-                    "File Create Serializer validation error",
-                    "No files provided",
-                ),
-            )
         for file_path in files:
             file_instance = File(author=user, file=file_path)
             file_instance.save()
